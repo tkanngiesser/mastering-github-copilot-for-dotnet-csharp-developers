e6dd5fab1a8ad99fe16e0c9a5da8b448a9721267
Tino Kanngiesser <tinokanngiesser@gmail.com>
2025-02-27 22:20:50 +0000

    add Skills project with basic calculator functionality

---
+++ code/Skills/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Calculate()
+{
+    Console.WriteLine("Enter the first number");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the operation");
+    string? input = Console.ReadLine();
+    string operation = input ?? string.Empty;
+    if (operation == "+")
+    {
+        Console.WriteLine(num1 + num2);
+    }
+    else if (operation == "-")
+    {
+        Console.WriteLine(num1 - num2);
+    }
+    else if (operation == "*")
+    {
+        Console.WriteLine(num1 * num2);
+    }
+    else if (operation == "/")
+    {
+        Console.WriteLine(num1 / num2);
+    }
+    else
+    {
+        Console.WriteLine("Invalid operation");
+    }
+}
+
+Calculate();
